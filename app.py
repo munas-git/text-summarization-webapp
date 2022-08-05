bf2769abd59d7509192c63cd9a7a658fd79463a6
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-08-05 22:19:12 +0100

    Changed post first to get, Centralized text area.

--- app.py
+++ app.py
@@ -5,15 +5,20 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 # app.secret_key = "work"
 
-@app.route('/home/', methods= ['POST', 'GET'])
+@app.route('/home/', methods= ['GET', 'POST'])
 def first():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        return render_template('test.html')
+        # name = request.form['text']
+        # print(name)
+        # # render_template('test.html', result= name)
+        # return redirect(url_for('second', txt=name))
+    elif request.method == 'GET':
         name = request.form['text']
         print(name)
         # render_template('test.html', result= name)
         return redirect(url_for('second', txt=name))
-    else:
-        return render_template('test.html')
+        # return render_template('test.html')
 
 
 @app.route('/<txt>/', methods=['POST', 'GET'])
