68d395ef64dfd3c56aee279ce2c6d70d94992de5
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2022-08-05 22:41:26 +0100

    Front-end prototype ready

--- app.py
+++ app.py
@@ -3,28 +3,15 @@ from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
-# app.secret_key = "work"
 
-@app.route('/home/', methods= ['GET', 'POST'])
+
+@app.route('/summarize/', methods= ['GET', 'POST'])
 def first():
     if request.method == 'GET':
         return render_template('test.html')
-        # name = request.form['text']
-        # print(name)
-        # # render_template('test.html', result= name)
-        # return redirect(url_for('second', txt=name))
-    elif request.method == 'GET':
-        name = request.form['text']
-        print(name)
-        # render_template('test.html', result= name)
-        return redirect(url_for('second', txt=name))
-        # return render_template('test.html')
-
-
-@app.route('/<txt>/', methods=['POST', 'GET'])
-def second(txt):
-    
-    return render_template('test.html', result= txt)
+    elif request.method == 'POST':
+        name = request.form.get('text', None)
+        return render_template('test.html', final_summary=name)
 
 
 # run the server. 
