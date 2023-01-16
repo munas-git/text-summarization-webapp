38ebe4aa873e4576649ba51da6f81ebe7222e9d9
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2023-01-16 20:33:06 +0100

    Backend routing completely integrated with frontend

--- WebApp/app.py
+++ WebApp/app.py
@@ -8,10 +8,7 @@ app.secret_key = 'forsummary'
 
 @app.route('/', methods= ['GET'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
-    elif request.method == 'POST':
-        return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/text-summary', methods= ['GET', 'POST'])
