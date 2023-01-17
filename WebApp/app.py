18e5cd6e2341f56fc234b512ca3601d3e7ff4870
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2023-01-17 18:08:29 +0100

    File upload summary and text summary feature implemented.

--- WebApp/app.py
+++ WebApp/app.py
@@ -64,12 +64,50 @@ def doc_summary():
     if request.method == "GET":
         return render_template("docSummary.html")
     elif request.method == "POST":
+        # Getting document and churn level from form
         doc = request.files['uploadFile']
-        text = extract_txt(doc)
-        print(text)
-        print(doc.filename)
+        churn_level = float(request.form.get('churn_level'))
+
+        # Checking file extention
+        if doc.filename.split(".")[-1] == "txt":
+            original_text = extract_txt(doc)
+        elif doc.filename.split(".")[-1] in ["doc", "docx"]:
+            original_text = extract_docx(doc)
+
+        # Initializing summarizer.
+        summarize = Summarizer(original_text, churn_level)
+
+        # Converting original text to word and sentence tokens.
+        word_sentence_tokens = summarize.word_sentence_tokenizer()
         
-        return render_template("docSummary.html")
+        try:
+            # separating word and sentence tokens from word_sentence_tokens tuple.
+            sentence_tokens_list = word_sentence_tokens[0]
+            word_tokens_list = word_sentence_tokens[1]
+
+            # Calculating word scores, returning word scores and most occuring word.
+            word_frequency_scores_topic = word_count_vec(word_tokens_list)
+            word_frequency_scores = word_frequency_scores_topic[0]
+            topic = word_frequency_scores_topic[1].capitalize()
+
+            # Scoring sentences based on word_frequency_scores.
+            sentence_scores = sentence_scoring(sentence_tokens_list, word_frequency_scores)
+
+            # Returning top n sentences in the order which they appear in original text.
+            sorted_summary = summarize.summary_sorting(sentence_scores)
+
+            # Swapping selected synonyms and returning final_summary string.
+            final_summary = string_synonym_swap(sorted_summary)
+
+            # Getting final details
+            summary_length = len(final_summary)
+            original_text_length = len(original_text)
+
+            return render_template('result.html', final_summary = final_summary, topic = topic, original_text = original_text, summary_length = summary_length, original_text_length = original_text_length)
+        except Exception:
+            # Returns 'Summary Undetermined' to the final-summary field and 'Undetermined' for other summary information.
+            message_2 = "Undetermined"
+            return render_template('textSummary.html', final_summary = "Summary Undetermined", topic = message_2, original_text = original_text, summary_length = message_2, original_text_length = message_2)
 
 
 @app.route("/about/", methods= ["GET"])
