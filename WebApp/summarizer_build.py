0878799b6061534ab8ec93177065b7f821a37757
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2023-01-23 11:42:44 +0100

    Commit 4 in attempt to fix hosted version issue.

--- WebApp/summarizer_build.py
+++ WebApp/summarizer_build.py
@@ -3,8 +3,8 @@ import nltk
 import docx
 import pickle
 import pandas as pd
-# nltk.download('stopwords')
-# nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
