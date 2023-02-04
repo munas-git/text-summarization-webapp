1f43fdac87297c3446a7f0bf638fdd1d8aec1b01
Ebereonwu Einstein Munachiso <einsteinmunachiso@gmail.com>
2023-02-04 16:36:28 +0100

    Checcking what is wrong with absstractive summary

--- WebApp/summarizer_build.py
+++ WebApp/summarizer_build.py
@@ -1,12 +1,12 @@
 # Importing important libraries.
 import nltk
 import docx
-import torch
+# import torch
 import pickle
 import pandas as pd
 nltk.download('stopwords')
 nltk.download('punkt')
-from transformers import pipeline
+# from transformers import pipeline
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
@@ -200,9 +200,9 @@ def abs_summary(text:str, churn_level:float) -> str:
     # Calculating minimum and maximum summary length.
     min = int((churn_level * len(text.split())/2))
     max = int(churn_level * len(text.split()))
-    summarizer = pipeline("summarization", model= "sshleifer/distilbart-cnn-12-6")
-    summary = summarizer(text, min_length=min, max_length=max, do_sample = False)
-    summary = summary[0]["summary_text"].strip()
+    # summarizer = pipeline("summarization", model= "sshleifer/distilbart-cnn-12-6")
+    # summary = summarizer(text, min_length=min, max_length=max, do_sample = False)
+    summary = """summary[0]["summary_text"].strip()"""
     return summary
 
 
